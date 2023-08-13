8f6c781aed873b9368a3a576d7520e9229cb218a
gamerfoxx <admin@josephludbrook.com>
2023-08-13 14:05:14 -0400

    Add background image and gradient

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-	return <StartGameScreen />;
+	return (
+		<LinearGradient
+			colors={['#631574', '#eea41e']}
+			style={styles.mainScreen}>
+			<ImageBackground
+				source={require('./assets/Image/background.png')}
+				resizeMode="cover"
+				style={styles.mainScreen}
+				imageStyle={styles.backgroundImage}>
+				<StartGameScreen />
+			</ImageBackground>
+		</LinearGradient>
+	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	mainScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.15,
+	},
+});
